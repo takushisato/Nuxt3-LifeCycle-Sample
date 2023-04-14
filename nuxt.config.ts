cee7d4aa6a601ca9f10a1e5b992731f658a77b51
taku820801 <takuchanpower@gmail.com>
2023-04-15 08:54:06 +0900

    環境構築完

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-
+export default ({
+    ssr: true,
+    components: ['@/components', '@/components/parts', '@/components/blocks', '@/components/forms'],
+    modules: [],
+    srcDir: 'src/',
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,4 @@
+<script lang="ts"></script>
+<template>
+  <h1>test</h1>
+</template>
